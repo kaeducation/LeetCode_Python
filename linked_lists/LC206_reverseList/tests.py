ce03ec57c9be40d52b2b908420fa6c74cebc07b7
kevin nguyen <kevinrsdnguyen@gmail.com>
2019-06-29 14:08:21 -0700

    Started LL

---
+++ linked_lists/LC206_reverseList/tests.py
@@ -0,0 +1,27 @@
+import unittest
+from index import Solution
+
+obj = Solution()
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class IntervalTests(unittest.TestCase):
+
+    def test_1(self):
+        """reverses the list"""
+        head = ListNode("A")
+        b_node = ListNode("B")
+        head.next = b_node
+
+        self.assertEqual(obj.reverseList(head), b_node)
+        self.assertEqual(b_node.next, head)
+        self.assertEqual(head.next, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
