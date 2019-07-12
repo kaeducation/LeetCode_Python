3eb9ee4734ede56c221548137505bc119b457dc9
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-07-12 16:57:01 -0700

    spacing

--- strings/LC5_longestPalindrome/main.py
+++ strings/LC5_longestPalindrome/main.py
@@ -1,2 +1,8 @@
+# Given a string s, find the longest palindromic substring in s
+# --- Example
+# longestPalindrome("cbbd") --> "bb"
+# longestPalindrome("abba") --> "abba"
+# longestPalindrome("a") --> "a"
+
 class Solution:
     def longestPalindrome(self, s):
