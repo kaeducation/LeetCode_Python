9265532c663accb66faa6f785a05cc300f43f179
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-07-19 22:57:12 -0700

    Added directions for 322

--- intervals/LC252_canAttendMeetings/main.py
+++ intervals/LC252_canAttendMeetings/main.py
@@ -1,2 +1,10 @@
+# Given an array of meeting time intervals consisting of start and end
+# times [s1, e1], [s2, e2], ... , determine if a person could attend
+#  all meetings.
+# ---------------------------
+# canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> False
+# canAttendMeetings([[2, 4], [7, 10]]) --> True
+
+
 class Solution:
     def canAttendMeetings(self, intervals):
