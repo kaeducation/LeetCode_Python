d993f1a6983f69af8d91686b5f4127ccbdcb8c35
kevin nguyen <kevinrsdnguyen@gmail.com>
2019-08-14 15:15:27 -0700

    Made practice tests more strict

--- practice/detect_cycle_directed/tests.py
+++ practice/detect_cycle_directed/tests.py
@@ -6,13 +6,6 @@ obj = Solution()
 
 class PracticeTests(unittest.TestCase):
 
-    def test_1(self):
-        """returns True correctly"""
-        self.assertEqual(obj.hasCycle(
-            1, [[0, 0]]), True)
-        # self.assertEqual(obj.hasCycle(
-        #     4, [[0, 1], [0, 2], [1, 2], [2, 0], [2, 3], [3, 3]]), True)
-
     def test_2(self):
         """returns False correctly"""
         self.assertEqual(obj.hasCycle(
@@ -24,6 +17,17 @@ class PracticeTests(unittest.TestCase):
         #  |  \
         #  v    >
         #  1 ->  2
+
+    def test_1(self):
+        """returns True correctly"""
+        self.assertEqual(obj.hasCycle(
+            1, [[0, 0]]), True)
+
+        self.assertEqual(obj.hasCycle(
+            3, [[0, 1], [2, 1], [2, 2]]), True)
+
+        self.assertEqual(obj.hasCycle(
+            4, [[0, 1], [0, 2], [1, 2], [2, 0], [2, 3], [3, 3]]), True)
 
 
 if __name__ == '__main__':
--- practice/detect_cycle_undirected/tests.py
+++ practice/detect_cycle_undirected/tests.py
@@ -9,11 +9,15 @@ class PracticeTests(unittest.TestCase):
     def test_1(self):
         """returns False correctly"""
         self.assertEqual(obj.hasCycle(2, [[0, 1]]), False)
+        self.assertEqual(obj.hasCycle(3, [[0, 1], [1, 2]]), False)
 
     def test_2(self):
         """returns True correctly"""
         self.assertEqual(obj.hasCycle(
             4, [[1, 0], [0, 3], [1, 2], [2, 3]]), True)
+
+        self.assertEqual(obj.hasCycle(
+            5, [[1, 0], [0, 2], [2, 1], [0, 3], [3, 4]]), True)
 
 
 if __name__ == '__main__':
