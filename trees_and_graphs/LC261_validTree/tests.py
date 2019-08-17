911ae4f7ad2c244a7ada2426d02d93f64034150c
kevin nguyen <kevinrsdnguyen@gmail.com>
2019-08-17 15:10:06 -0700

    Added LC261

---
+++ trees_and_graphs/LC261_validTree/tests.py
@@ -0,0 +1,27 @@
+import unittest
+from main import Solution
+
+obj = Solution()
+
+
+class IntervalTests(unittest.TestCase):
+
+    def test_1(self):
+        """Returns True correctly"""
+        self.assertEqual(obj.validTree(
+            5,  [[0, 1], [0, 2], [0, 3], [1, 4]]), True)
+        self.assertEqual(obj.validTree(
+            5, [[1, 0], [0, 2], [0, 3], [3, 4]]), True)
+
+    def test_2(self):
+        """Returns False due to seperate components"""
+        self.assertEqual(obj.validTree(4, [[0, 1], [2, 3]]), False)
+
+    def test_3(self):
+        """Returns False due to cycle"""
+        self.assertEqual(obj.validTree(
+            5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
