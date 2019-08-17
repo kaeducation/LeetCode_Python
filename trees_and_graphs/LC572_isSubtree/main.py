cf9e60cf601785f33fe55892ffe2ae7b9172d6b4
kevin nguyen <kevinrsdnguyen@gmail.com>
2019-08-16 17:18:54 -0700

    removed sol

--- trees_and_graphs/LC572_isSubtree/main.py
+++ trees_and_graphs/LC572_isSubtree/main.py
@@ -1,21 +1,2 @@
 class Solution():
     def isSubtree(self, s, t):
-
-        def sametree(p, q):
-            if p and q:
-                return p.val == q.val and sametree(p.left, q.left) and sametree(p.right, q.right)
-            return p == q
-
-        stack = [s]
-
-        while stack:
-            popped = stack.pop()
-
-            if sametree(popped, t):
-                return True
-            if popped.left:
-                stack.append(popped.left)
-            if popped.right:
-                stack.append(popped.right)
-
-        return False
