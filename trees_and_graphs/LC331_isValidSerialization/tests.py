08251f395b20c2f662a961e5c4c684ad5f84bf0f
kevin nguyen <kevinrsdnguyen@gmail.com>
2019-08-20 17:18:28 -0700

    Added LC331

---
+++ trees_and_graphs/LC331_isValidSerialization/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from main import Solution
+
+obj = Solution()
+
+
+class TreeTests(unittest.TestCase):
+
+    def test_1(self):
+        """Returns True correctly"""
+        self.assertEqual(obj.isValidSerialization("9,#,92,#,#"), True)
+
+    def test_2(self):
+        """Returns False correctly"""
+        self.assertEqual(obj.isValidSerialization("9,#,92,#"), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
