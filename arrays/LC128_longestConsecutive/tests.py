cab3f2427fe91b516b2a9474ff53061111d423d4
kevin nguyen <kevinrsdnguyen@gmail.com>
2019-08-23 13:26:07 -0700

    Added LC128

---
+++ arrays/LC128_longestConsecutive/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from main import Solution
+
+obj = Solution()
+
+
+class ArrayTests(unittest.TestCase):
+
+    def test_1(self):
+        """Works"""
+        self.assertEqual(obj.longestConsecutive([100, 4, 200, 1, 3, 2]), 4)
+        self.assertEqual(obj.longestConsecutive([100, 4, 200, 1, 2]), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
