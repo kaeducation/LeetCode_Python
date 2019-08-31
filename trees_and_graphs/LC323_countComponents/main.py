18808086767676f30b124674a4648879d8430d0d
kevinrsdnguyen <kevinrsdnguyen@gmail.com>
2019-08-30 22:31:55 -0700

    Reformatted directions for 323

--- trees_and_graphs/LC323_countComponents/main.py
+++ trees_and_graphs/LC323_countComponents/main.py
@@ -1,5 +1,6 @@
-# Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes),
-# write a function to find the number of connected components in an undirected graph.
+# Given n nodes labeled from 0 to n - 1 and a list of undirected edges
+# (each edge is a pair of nodes), write a function to find the number
+# of connected components in an undirected graph.
 
 # Example 1:
 #  0          3
